0d91da23ef0e7b33a8e9d319e32a8e0c555772aa
Sy Le <lenguyensy@gmail.com>
2023-11-11 07:40:34 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 echo '>>> install.sh'
 date >> /tmp/setup.txt
 echo 'Running install.sh' >> /tmp/setup.txt
+
+curl -s https://raw.githubusercontent.com/synle/bashrc/master/.build/gitconfig > "$HOME/.gitconfig"
+curl -s https://raw.githubusercontent.com/synle/bashrc/master/.build/vimrc > "$HOME/.vimrc"
