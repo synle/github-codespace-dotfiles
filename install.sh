76d76c3a87d825b6a2ebe9008cce5de4e27a80f0
Sy Le <lenguyensy@gmail.com>
2026-03-10 14:13:13 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,10 @@ curl -s "$BASE_URL/.build/vimrc"     > "$HOME/.vimrc"
 curl -s "$BASE_URL/.build/inputrc"   > "$HOME/.inputrc"
 curl -s "$BASE_URL/software/bootstrap/profile-core.sh" > "$HOME/.bash_syle"
 
+# install fzf
+if [ ! -d "$HOME/.fzf" ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+  "$HOME/.fzf/install" --all --no-bash --no-zsh --no-fish
+fi
+
 grep -qF '.bash_syle' "$HOME/.bashrc" 2>/dev/null || echo '[ -f "$HOME/.bash_syle" ] && source "$HOME/.bash_syle"' >> "$HOME/.bashrc"
