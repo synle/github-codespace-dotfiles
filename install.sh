04ee093270364b2e87af4212fc0f9e2b3f749d56
Sy Le <lenguyensy@gmail.com>
2026-03-10 14:11:52 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@ echo '>>> install.sh'
 date >> /tmp/setup.txt
 echo 'Running install.sh' >> /tmp/setup.txt
 
-curl -s https://raw.githubusercontent.com/synle/bashrc/master/.build/gitconfig > "$HOME/.gitconfig"
-curl -s https://raw.githubusercontent.com/synle/bashrc/master/.build/vimrc > "$HOME/.vimrc"
+BASE_URL="https://raw.githubusercontent.com/synle/bashrc/master"
 
-echo '''
-alias g='git'
-''' >> ~/.bashrc
+curl -s "$BASE_URL/.build/gitconfig" > "$HOME/.gitconfig"
+curl -s "$BASE_URL/.build/vimrc"     > "$HOME/.vimrc"
+curl -s "$BASE_URL/.build/inputrc"   > "$HOME/.inputrc"
+curl -s "$BASE_URL/software/bootstrap/profile-core.sh" > "$HOME/.bash_syle"
+
+grep -qF '.bash_syle' "$HOME/.bashrc" 2>/dev/null || echo '[ -f "$HOME/.bash_syle" ] && source "$HOME/.bash_syle"' >> "$HOME/.bashrc"
